553209707f4284928487cbe3e3d559b68b141823
Michael Su <mailmsuu@gmail.com>
2022-04-07 15:56:48 +0100

    Added web_helpers to DRY up code

--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -1,19 +1,13 @@
 feature 'Enter player names' do
   scenario 'Check player names can be entered' do
-  visit('/')
-  fill_in :player_1, with: 'David'
-  fill_in :player_2, with: 'Michael'
-  click_button 'Submit'
-  expect(page).to have_content "player 1: David\nplayer 2: Michael"
+    sign_in_and_play
+    expect(page).to have_content "player 1: David\nplayer 2: Michael"
   end
 end
 
 feature 'See player hit points' do
   scenario 'See hit points of player 2' do
-  visit('/')
-  fill_in :player_1, with: 'David'
-  fill_in :player_2, with: 'Michael'
-  click_button 'Submit'
-  expect(page).to have_content "Michael HP: 10"
+    sign_in_and_play
+    expect(page).to have_content "Michael HP: 10"
   end
 end
