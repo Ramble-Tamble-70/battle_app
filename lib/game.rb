f606b1eb228910c655fe69d7c64597b4e8690dbe
David Ewan Campbell <ewan.david@gmail.com>
2022-04-08 17:23:58 +0100

    Resolving switching turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,13 @@
 require_relative './player'
 
 class Game
-  attr_reader :player_1, :player_2, :next_turn_v
+  attr_reader :player_1, :player_2, :next_turn_v, :current_turn_v
 
   def initialize(player_1 = Player.new('player_1'), player_2 = Player.new('player_2'))
     @player_1 = player_1
     @player_2 = player_2
     @next_turn_v = @player_1
+    @current_turn_v = @player_2
   end
 
   def attack(player_attacked)
@@ -14,11 +15,12 @@ class Game
     next_turn
   end
   
-  def next_turn 
-
+  def next_turn
     if @next_turn_v == @player_1
+       @current_turn_v = @player_1
        @next_turn_v = @player_2
     else
+       @current_turn_v = @player_2
        @next_turn_v = @player_1
     end
   end
